bbdd9dcb04fa55191503f7acf362f6f93d20855a
Kureans <leonardtengys@gmail.com>
2023-03-12 23:16:26 +0800

    Integrate fetch request logic into form and display generated URL

--- frontend/src/components/URLInputFormComponent.tsx
+++ frontend/src/components/URLInputFormComponent.tsx
@@ -1,15 +1,51 @@
 import { Form, Button, Card } from "react-bootstrap";
+import { useState } from 'react';
 
 function URLInputForm() {
+
+  const [urlToShorten, setUrlToShorten] = useState("");
+  const [shortenedUrl, setShortenedUrl] = useState("");
+
+  let handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    try {
+      let res = await fetch("http://localhost:3000/shorten", {
+        mode: "cors",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ 
+          longUrl: urlToShorten 
+        }),
+      });
+
+      let resText = await res.text();
+      if (res.status === 201) {
+        setShortenedUrl(resText);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <Card className="InputFormCard bg-dark text-white">
-      <Form className="InputForm">
+      <Form className="InputForm" onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Shorten any URL you want!</Form.Label>
-          <Form.Control type="email" placeholder="Input Link here!" />
+          <Form.Control 
+          type="url" 
+          placeholder="Input Link here!" 
+          value={urlToShorten}
+          onChange={(e) => setUrlToShorten(e.target.value)}/>
         </Form.Group>
         <Button className="UrlConvertButton" variant="primary" type="submit">Shorten</Button>
       </Form>
+      {shortenedUrl && (
+        <p>Here is your URL: <a href={shortenedUrl} target="_blank">{shortenedUrl}</a></p>
+      )}
     </Card>
   );
 }
